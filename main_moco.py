f730ca86e89fb7b880c8954e543d98ec63053cf0
Shuai Liu <1170500804hit@gmail.com>
2020-07-14 18:18:55 +0800

    ...

--- main_moco.py
+++ main_moco.py
@@ -25,16 +25,16 @@ import torchvision.models as models
 import moco.loader
 import moco.builder
 from Datasets import Rolling_Window_Year_Dataset
-def setup(rank, world_size):
-    os.environ['MASTER_ADDR'] = 'localhost'
-    os.environ['MASTER_PORT'] = '12355'
-
-    # initialize the process group
-    dist.init_process_group("gloo", rank=rank, world_size=world_size)
-
-
-def cleanup():
-    dist.destroy_process_group()
+# def setup(rank, world_size):
+#     os.environ['MASTER_ADDR'] = 'localhost'
+#     os.environ['MASTER_PORT'] = '12355'
+#
+#     # initialize the process group
+#     dist.init_process_group("gloo", rank=rank, world_size=world_size)
+# 
+#
+# def cleanup():
+#     dist.destroy_process_group()
 
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
