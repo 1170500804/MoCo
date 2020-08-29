fed6855496573ffd0736ffcefbc1daf1d8892d85
Shuai Liu <1170500804hit@gmail.com>
2020-08-29 15:37:11 +0800

    dataset

---
+++ get_foundation_csv.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import os
+import random
+image_folder = '/home/saschaho/Simcenter/Foundation_Images_Orig'
+dataset = {}
+dataset['filename'] =  []
+dataset['year_built'] = []
+min_year = 1913
+max_year = 2013
+for type_ in os.listdir(image_folder):
+    for img in os.listdir(type_):
+        if(img.endswith('.jpg')):
+            dataset['filename'].append(os.path.join(type_,img))
+            dataset['year_built'].append(random.randint(1913, 2013))
+dataset_df = pd.DataFrame(dataset)
+dataset_df.to_csv('foundation_type')
