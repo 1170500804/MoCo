a2f32b2eeb3f2a5088554e4376cda936e92f8f7b
Shuai Liu <1170500804hit@gmail.com>
2020-09-06 01:17:04 +0800

    details

--- evaluate_embedding.py
+++ evaluate_embedding.py
@@ -70,9 +70,7 @@ def main():
     normalize = normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                          std=[0.229, 0.224, 0.225])
     augmentation_val = transforms.Compose([
-                transforms.Resize(256),
-                transforms.CenterCrop(224),
-                transforms.ToTensor(),
+                transforms.Resize(224),
                 normalize,
             ])
 
