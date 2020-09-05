f522308f2dcee9cbc6dae52ff71b141a50e0fe4c
Shuai Liu <1170500804hit@gmail.com>
2020-09-05 22:52:19 +0800

    no parallel

--- evaluate_embedding.py
+++ evaluate_embedding.py
@@ -1,9 +1,6 @@
 from __future__ import print_function
 import time
-import numpy as np
-import pandas as pd
-from sklearn.datasets import fetch_mldata
-from sklearn.decomposition import PCA
+
 from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 import seaborn as sns
