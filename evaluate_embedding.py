887d9ecdeae6a1cf896c854fd117709015591d58
Shuai Liu <1170500804hit@gmail.com>
2020-09-05 22:50:12 +0800

    no parallel

--- evaluate_embedding.py
+++ evaluate_embedding.py
@@ -1,5 +1,13 @@
+from __future__ import print_function
+import time
+import numpy as np
+import pandas as pd
+from sklearn.datasets import fetch_mldata
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import seaborn as sns
 import argparse
-from sklearn.manifold import t_sne
 import pandas as pd
 import os
 
@@ -15,6 +23,25 @@ from torchvision import models
 model_names = sorted(name for name in models.__dict__
     if name.islower() and not name.startswith("__")
     and callable(models.__dict__[name]))
+def plot_t_sne(data_subset, filename):
+    time_start = time.time()
+    tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
+    tsne_results = tsne.fit_transform(data_subset)
+    print('t-SNE done! Time elapsed: {} seconds'.format(time.time() - time_start))
+    data_subset['tsne-2d-one'] = tsne_results[:, 0]
+    data_subset['tsne-2d-two'] = tsne_results[:, 1]
+    plt.figure(figsize=(16, 10))
+    # sns.set()
+
+    sns_plot = sns.scatterplot(
+        x="tsne-2d-one", y="tsne-2d-two",
+        hue="year_built",
+        palette=sns.color_palette("hls", 10),
+        data=data_subset,
+        legend="full",
+        alpha=0.3
+    )
+    sns_plot.savefig(filename)
 def main():
     parser = argparse.ArgumentParser(description='PyTorch year_built Training Validation')
     parser.add_argument('--validate-data', type=str, help='the data to be visualized', required=True)
@@ -86,6 +113,14 @@ def main():
     if args.embedding_file:
         pass
     else:
+        if (args.to_csv):
+            name = args.resume.split('/')
+            if(args.resume.endswith('/')):
+                name = name[-3].split('_')[-1] + name[-2].split['.'][0]
+                print(name)
+            else:
+                name = name[-2].split('_')[-1] + name[-1].split['.'][0]
+            print(name)
         # compute embeddings
         model.eval()
         labels = None
@@ -121,14 +156,7 @@ def main():
 
         df = pd.DataFrame(embeddings)
         if (args.to_csv):
-            name = args.resume.split('/')
-            if(args.resume.endswith('/')):
-                name = name[-3].split('_')[-1] + name[-2].split['.'][0]
-                print(name)
-            else:
-                name = name[-2].split('_')[-1] + name[-1].split['.'][0]
-            print(name)
-            df.to_csv('/home/shuai/MoCo_stats/embedding/embeddings_'+args.resume+'.csv')
+            df.to_csv('/home/shuai/MoCo_stats/embedding/embeddings_'+name+'.csv')
 if __name__ == '__main__':
     if not os.path.exists('/home/shuai/MoCo_stats/embedding'):
         os.mkdir('/home/shuai/MoCo_stats/embedding')
